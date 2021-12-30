18affca76502b6a80f416bcf890131ca39a50ba1
seito.tanaka <seito.tanaka.dev@gmail.com>
2021-12-30 19:17:15 +0900

    Add timer symbol

--- test/syntax-tokenizer.test.js
+++ test/syntax-tokenizer.test.js
@@ -22,6 +22,38 @@ describe('syntax-tokenizer', () => {
     ]);
   });
 
+  test('tokenize the time with a symbol', () => {
+    expect(tokenize('p1✍️')).toStrictEqual([
+      {
+        type: tokenType.TIME,
+        timeMin: 1,
+        symbol: '✍️'
+      }
+    ]);
+  });
+
+  test('tokenize times with a symbol', () => {
+    expect(tokenize('(p1✍️ p2☕️)2')).toStrictEqual([
+      {
+        type: tokenType.OPEN_PARENTHESIS
+      },
+      {
+        type: tokenType.TIME,
+        timeMin: 1,
+        symbol: '✍️'
+      },
+      {
+        type: tokenType.TIME,
+        timeMin: 2,
+        symbol: '☕️'
+      },
+      {
+        type: tokenType.CLOSE_PARENTHESIS,
+        loop: 2
+      }
+    ]);
+  });
+
   test('returns empty when there is a syntax error', () => {
     expect(tokenize('p1 (p1 pa)')).toHaveLength(0);
   });
